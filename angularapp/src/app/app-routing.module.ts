4d6d291db74606a87e492862d03ee4649765f7fd
MirthulaRE <mirthulamirth@gmail.com>
2025-10-27 18:33:49 +0000

    routing module by Mirthula

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,16 +3,45 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminaddserviceComponent } from './components/adminaddservice/adminaddservice.component';
+import { AdminviewappointmentComponent } from './components/adminviewappointment/adminviewappointment.component';
+import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
+import { AdminviewserviceComponent } from './components/adminviewservice/adminviewservice.component';
+import { AdminviewuserdetailsComponent } from './components/adminviewuserdetails/adminviewuserdetails.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UseraddappointmentComponent } from './components/useraddappointment/useraddappointment.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UserviewappointmentComponent } from './components/userviewappointment/userviewappointment.component';
+import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
+import { ErrorComponent } from './components/error/error.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AdminnavbarComponent } from './components/adminnavbar/adminnavbar.component';
+import { UsernavbarComponent } from './components/usernavbar/usernavbar.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}, 
-  { path: 'register', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent },
-
+  {path:'',component:HomeComponent,redirectTo:'full'}, 
+  {path:'home',component:HomeComponent},
+  {path: 'registration', component: RegistrationComponent },
+  {path: 'login', component: LoginComponent },
+  {path:'adminaddservice',component:AdminaddserviceComponent},
+  {path:'adminviewappointment',component:AdminviewappointmentComponent},
+  {path:'adminviewfeedback',component:AdminviewfeedbackComponent},
+  {path:'adminviewservice',component:AdminviewserviceComponent},
+  {path:'adminviewuserdetails',component:AdminviewuserdetailsComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'useraddappointment',component:UseraddappointmentComponent},
+  {path:'useraddfeedback',component:UseraddfeedbackComponent},
+  {path:'userviewappointment',component:UserviewappointmentComponent},
+  {path:'userviewfeedback',component:UserviewfeedbackComponent},
+  {path:'error',component:ErrorComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'adminnavbar',component:AdminnavbarComponent},
+  {path:'usernavbar',component:UsernavbarComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
