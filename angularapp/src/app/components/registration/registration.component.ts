77d6751b34ca5f231bf083ae3b847198e7e6208f
MirthulaRE <mirthulamirth@gmail.com>
2025-10-28 11:38:26 +0000

    authservice

--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,25 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  user={
-    username:'',
-    email:'',
-    mobileNumber:'',
-    role:'',
-    password:'',
-    confirmPassword:''
-
-  }
-  
-  ngOnInit(): void {
-   
-  }
-
-  
-
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+ 
 }
--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,30 +1,116 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+//   register(value: any) {
+//     throw new Error('Method not implemented.');
+//   }
+//   constructor() { }
+
+//   logout() {
+      
+//   }
+//   getRole():string {
+//     return 'user';
+      
+//   }
+
+  
+//   getAuthenticatedUser(){
+
+//   }
+
+
+//   isLoggedIn():boolean {
+//     return false;
+      
+//   }
+// }
+
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+
+export const AUTHENTICATED_USER = 'authenticatedUser';
+export const TOKEN = 'token';
+export const PAGE_ID = 'pageId';
+export const USER_ID = 'userId';
+export const ROLE = 'role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() { }
 
-  logout() {
-      
-  }
-  getRole():string {
-    return 'user';
-      
+
+  public baseUrl = "https://8080-acedbabebccdb334276216cebfbccone.premiumproject.examly.io/api/user";
+
+ 
+  constructor(private http: HttpClient) { }
+
+  register(user : User) : Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`,user);
   }
 
+  login(username: string, password: string): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/login`, { username, password }).pipe(
+      map(
+        data => {
+          localStorage.setItem(USER_ID, "" + data.userId);
+          localStorage.setItem(AUTHENTICATED_USER, username);
+          localStorage.setItem(TOKEN, `Bearer ${data.token}`);
+          localStorage.setItem(ROLE, data.userRole);
+          return data;
+        }
+      )
+    );
+  }
   
-  getAuthenticatedUser(){
-
+  getRole(): string {
+    return this.getAuthenticatedRole();
   }
 
-
-  isLoggedIn():boolean {
-    return false;
-      
+  isLoggedin(): boolean {
+    let user = localStorage.getItem(AUTHENTICATED_USER);
+    return !(user == null);
   }
-  
+  logout(): void { localStorage.clear(); }
+  isAdmin(): boolean { return this.getAuthenticatedRole() === 'ADMIN'; }
+  isUser(): boolean { return this.getAuthenticatedRole() === 'USER'; }
+
+  getAuthenticatedUserId(): number {
+    return parseInt(localStorage.getItem(USER_ID) || "0");
+  }
+
+  getAuthenticatedUser() {
+    return localStorage.getItem(AUTHENTICATED_USER);
+  }
+
+  getAuthenticatedRole() {
+    return localStorage.getItem(ROLE);
+  }
+
+  getAuthenticatedToken() {
+    if (this.getAuthenticatedUser())
+      return localStorage.getItem(TOKEN);
+  }
+
+  pageId(): string {
+    var pageId = localStorage.getItem(PAGE_ID);
+    if (pageId === null) {
+      localStorage.setItem(PAGE_ID, 'login');
+    }
+    return pageId;
+  }
+
+  setPageId(pageId: string) {
+    localStorage.setItem(PAGE_ID, pageId);
+  }
 
 }
 
