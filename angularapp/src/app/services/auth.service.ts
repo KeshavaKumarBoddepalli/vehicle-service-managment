298ff5059a9f1b99eda802772a07e577a6252904
Suthicsha <suthicsharajesh001@gmail.com>
2025-10-28 11:18:26 +0000

    register and login component by suthi

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  register(value: any):Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  login(username: string, password: string):Observable<any> {
+    throw new Error('Method not implemented.');
+  }
   constructor() { }
 
   logout() {
