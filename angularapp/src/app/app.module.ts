f4a85f4e8c23a63499b0165ef9a05023480e8e1d
Suthicsha <suthicsharajesh001@gmail.com>
2025-10-29 07:21:58 +0000

    Register changes by suthi

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminaddserviceComponent } from './components/adminaddservice/adminaddservice.component';
@@ -12,7 +11,6 @@ import { AdminviewuserdetailsComponent } from './components/adminviewuserdetails
 import { AuthguardComponent } from './components/authguard/authguard.component';
 import { ErrorComponent } from './components/error/error.component';
 import { HomeComponent } from './components/home/home.component';
-//import { LginComponent } from './components/lgin/lgin.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -22,7 +20,7 @@ import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfe
 import { UsernavbarComponent } from './components/usernavbar/usernavbar.component';
 import { UserviewappointmentComponent } from './components/userviewappointment/userviewappointment.component';
 import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -53,7 +51,8 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -12,63 +12,61 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegistrationComponent implements OnInit {
   registerForm: FormGroup;
-   users: User[] = [];
+  users: User[] = [];
  
-   constructor(private fb: FormBuilder, private service: AuthService, private router: Router) {
-     this.registerForm = this.fb.group({
-       username: ['', [
-         Validators.required,
-         Validators.minLength(5),
-         Validators.maxLength(10),
-         Validators.pattern('^[A-Za-z][A-Za-z0-9_]*$')
-       ]],
-       email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
-       password: ['', [
-         Validators.required,
-         Validators.minLength(8),
-         Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
-       ]],
-       confirmPassword: ['', Validators.required],
-       mobileNumber: ['', [
-         Validators.required,
-         Validators.pattern('^[6-9]\\d{9}$')
-       ]],
-       userRole: ['', Validators.required]
-     },
-       {
-         validators: this.passwordMatchValidator
-       });
-   }
+  constructor(private fb: FormBuilder, private service: AuthService, private router: Router) {
+    this.registerForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+        Validators.pattern('^[A-Za-z][A-Za-z0-9_]*$')
+      ]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+      ]],
+      confirmPassword: ['', Validators.required],
+      mobileNumber: ['', [
+        Validators.required,
+        Validators.pattern('^[6-9]\\d{9}$')
+      ]],
+      userRole: ['', Validators.required]
+    },
+      {
+        validators: this.passwordMatchValidator
+      });
+  }
  
   ngOnInit(): void { }
  
-   passwordMatchValidator(form: AbstractControl) {
-     const password = form.get('password')?.value;
-     const confirmPassword = form.get('confirmPassword')?.value;
-     return password === confirmPassword ? null : { mismatch: true };
-   }
+  passwordMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
  
   onSubmit() {
     if (this.registerForm.valid) {
-     this.service.register(this.registerForm.value).subscribe(
-         (result) => {
-           this.registerForm.reset();
-           alert("Registration successful");
-           this.router.navigate(['/login']);
-         },
-         (error) => {
-           this.registerForm.reset();
-           alert("Registration not done");
-           this.router.navigate(['/error'], {
-             queryParams: { errorMsg: 'Registration not done due to existing emailId' }
-           });
-         }
-       );
+      this.service.register(this.registerForm.value).subscribe(
+        (result) => {
+          this.registerForm.reset();
+          alert("Registration successful");
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.registerForm.reset();
+          alert("Registration not done");
+          this.router.navigate(['/error'], {
+            queryParams: { errorMsg: 'Registration not done due to existing emailId' }
+          });
+        }
+      );
  
-     } else {
-       this.registerForm.markAllAsTouched();
-     }
-   }
-     
-
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
