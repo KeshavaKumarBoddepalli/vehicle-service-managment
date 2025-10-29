918187a33d2d2f217720fdd24a2cf001800b5fcc
MirthulaRE <mirthulamirth@gmail.com>
2025-10-29 12:38:07 +0000

    home admin user

--- angularapp/src/app/components/home/home.component.ts
+++ angularapp/src/app/components/home/home.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css']
-// })
-
-// export class HomeComponent {
-//   userRole: string = ''; 
-
-//   constructor() {  
-//     this.userRole = localStorage.getItem('role') || '';
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
